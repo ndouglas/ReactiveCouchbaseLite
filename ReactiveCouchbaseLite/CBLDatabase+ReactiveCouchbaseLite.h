09c1504ef0ff4ae808dbb047020c13dc6ef9f585
Nathan Douglas <ndouglas@devontechnologies.com>
2014-11-18 14:58:02 -0600

    Tidy.

--- ReactiveCouchbaseLite/CBLDatabase+ReactiveCouchbaseLite.h
+++ ReactiveCouchbaseLite/CBLDatabase+ReactiveCouchbaseLite.h
@@ -17,8 +17,9 @@
 @interface CBLDatabase (ReactiveCouchbaseLite)
 
 /**
-  When a new revision is added to the database, it receives a new sequence number; this can be used to check whether the
-  database has changed between two points in time.
+  When a new revision is added to the database, it receives a new sequence number; 
+  this can be used to check whether the database has changed between two points 
+  in time.
   
   @return A signal of NSNumber instances containing the last sequence number.
  */
