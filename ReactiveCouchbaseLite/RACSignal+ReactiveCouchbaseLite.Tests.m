5009ea80d962148e431352f53d80c39fbd34966c
Nathan Douglas <ndouglas@devontechnologies.com>
2014-11-19 12:34:11 -0600

    Fixes tests.

--- ReactiveCouchbaseLite/RACSignal+ReactiveCouchbaseLite.Tests.m
+++ ReactiveCouchbaseLite/RACSignal+ReactiveCouchbaseLite.Tests.m
@@ -16,36 +16,18 @@
 
 @implementation RACSignal_ReactiveCouchbaseLiteTests
 
-+ (void)setUp {
-	NDD_TRACE_ENTRY
-	DTS_TEST_CLASS_START
-	NDD_TRACE_EXIT
-}
-
-+ (void)tearDown {
-	NDD_TRACE_ENTRY
-	DTS_TEST_CLASS_END
-	NDD_TRACE_EXIT
-}
-
 - (void)setUp {
-	NDD_TRACE_ENTRY
 	[super setUp];
-	NDD_TRACE_EXIT
 }
 
 - (void)tearDown {
-	NDD_TRACE_ENTRY
 	[super tearDown];
-	NDD_TRACE_EXIT
 }
 
 - (void)test {
-	NDD_TRACE_ENTRY
 	/*
 		Run a test here.
 	*/
-	NDD_TRACE_EXIT
 }
 
 @end
