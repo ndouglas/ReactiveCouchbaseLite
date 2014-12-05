27fae522a1578c5094e16df4342add8c52573f3a
Nathan Douglas <ndouglas@devontechnologies.com>
2014-12-05 12:52:53 -0600

    Improves error definitions.

--- ReactiveCouchbaseLite/RCLDefinitions.m
+++ ReactiveCouchbaseLite/RCLDefinitions.m
@@ -9,25 +9,43 @@
 
 #import "RCLDefinitions.h"
 
-#undef NDD_OPTION_BASE_DEBUG_TRACE_LOCAL
-#define NDD_OPTION_BASE_DEBUG_TRACE_LOCAL										FALSE
-
 NSString * const RCLErrorDomain = @"RCLErrorDomain";
 
 extern NSError *RCLErrorWithCode(RCLErrorCodeType code) {
     NSString *description = nil;
     switch (code) {
         case RCLErrorCode_DocumentCouldNotBeFoundOrCreated:
-            description = NSLocalizedString(@"Couchbase-Lite could not find or create the requested document.", #code);
+            description = NSLocalizedString(@"Couchbase-Lite could not find or create the requested document.", @"RCLErrorCode_DocumentCouldNotBeFoundOrCreated");
             break;
         case RCLErrorCode_DocumentCouldNotBeFound:
-            description = NSLocalizedString(@"Couchbase-Lite could not find the requested document.", #code);
+            description = NSLocalizedString(@"Couchbase-Lite could not find the requested document.", @"RCLErrorCode_DocumentCouldNotBeFound");
             break;
         case RCLErrorCode_DocumentCouldNotBeCreated:
-            description = NSLocalizedString(@"Couchbase-Lite could not create the requested document.", #code);
+            description = NSLocalizedString(@"Couchbase-Lite could not create the requested document.", @"RCLErrorCode_DocumentCouldNotBeCreated");
             break;
         case RCLErrorCode_LocalDocumentCouldNotBeFound:
-            description = NSLocalizedString(@"Couchbase-Lite could not find the requested local document.", #code);
+            description = NSLocalizedString(@"Couchbase-Lite could not find the requested local document.", @"RCLErrorCode_LocalDocumentCouldNotBeFound");
+            break;
+        case RCLErrorCode_ViewCouldNotBeFound:
+            description = NSLocalizedString(@"Couchbase-Lite could not find the requested view.", @"RCLErrorCode_ViewCouldNotBeFound");
+            break;
+        case RCLErrorCode_ValidationCouldNotBeFound:
+            description = NSLocalizedString(@"Couchbase-Lite could not find the requested validation.", @"RCLErrorCode_ValidationCouldNotBeFound");
+            break;
+        case RCLErrorCode_FilterCouldNotBeFound:
+            description = NSLocalizedString(@"Couchbase-Lite could not find the requested filter.", @"RCLErrorCode_FilterCouldNotBeFound");
+            break;
+        case RCLErrorCode_TransactionWasNotCommitted:
+            description = NSLocalizedString(@"Couchbase-Lite could not commit the transaction.", @"RCLErrorCode_TransactionWasNotCommitted");
+            break;
+        case RCLErrorCode_RevisionCouldNotBeFound:
+            description = NSLocalizedString(@"Couchbase-Lite could not find the requested revision.", @"RCLErrorCode_RevisionCouldNotBeFound");
+            break;
+        case RCLErrorCode_AttachmentCouldNotBeFound:
+            description = NSLocalizedString(@"Couchbase-Lite could not find the requested attachment.", @"RCLErrorCode_AttachmentCouldNotBeFound");
+            break;
+        case RCLErrorCode_QueryRowCouldNotBeFound:
+            description = NSLocalizedString(@"Couchbase-Lite could not find the requested query row.", @"RCLErrorCode_QueryRowCouldNotBeFound");
             break;
     }
     return [NSError errorWithDomain:RCLErrorDomain code:code userInfo:@{
