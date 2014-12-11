7de37b61c4a192b7cc6fb3b2fe94555f0622008b
Nathan Douglas <ndouglas@devontechnologies.com>
2014-12-11 13:10:50 -0600

    Fixes issues with CBLLiveQuery (ReactiveCouchbaseLite).

--- ReactiveCouchbaseLite/CBLLiveQuery+ReactiveCouchbaseLite.Tests.m
+++ ReactiveCouchbaseLite/CBLLiveQuery+ReactiveCouchbaseLite.Tests.m
@@ -11,7 +11,10 @@
 #import "RCLTestDefinitions.h"
 #import "RCLDefinitions.h"
 
-@interface CBLLiveQuery_ReactiveCouchbaseLiteTests : XCTestCase
+@interface CBLLiveQuery_ReactiveCouchbaseLiteTests : XCTestCase {
+    CBLManager *_manager;
+    NSString *_databaseName;
+}
 
 @end
 
@@ -19,20 +22,108 @@
 
 - (void)setUp {
 	[super setUp];
+    _manager = [CBLManager sharedInstance];
+    _databaseName = [NSString stringWithFormat:@"test_%@", @([[[NSUUID UUID] UUIDString] hash])];
 }
 
 - (void)tearDown {
+    [[_manager databaseNamed:_databaseName error:NULL] deleteDatabase:NULL];
 	[super tearDown];
 }
 
-- (void)test {
-	/*
-		Run a test here.
-	*/
+- (void)asynchronouslyPostTrivialChangeToDocumentWithID:(NSString *)ID {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[[[CBLManager sharedInstance] databaseNamed:_databaseName error:NULL] documentWithID:ID] putProperties:@{} error:NULL];
+        });
+    });
+}
+
+- (void)testRows {
+    RACSignal *liveQuerySignal = [[[_manager rcl_databaseNamed:_databaseName]
+    flattenMap:^RACSignal *(CBLDatabase *database) {
+        return [database rcl_allDocumentsQuery];
+    }]
+    flattenMap:^RACSignal *(CBLQuery *query) {
+        CBLLiveQuery *liveQuery = query.asLiveQuery;
+        return [liveQuery rcl_rows];
+    }];
+    [self rcl_expectCompletionFromSignal:[[liveQuerySignal
+    doCompleted:^{
+        XCTFail(@"This signal is not supposed to complete.");
+    }]
+    takeUntilBlock:^BOOL (CBLQueryEnumerator *rows) {
+        BOOL result = rows.allObjects.count == 0;
+        NSLog(@"Filter condition %@matched for value %@ (%@).", result ? @"" : @"un", rows, rows.allObjects);
+        return result;
+    }]
+    timeout:5.0 description:@"observed initial value for rows"];
+    [self asynchronouslyPostTrivialChangeToDocumentWithID:[[NSUUID UUID] UUIDString]];
+    [self rcl_expectCompletionFromSignal:[[[liveQuerySignal
+    doCompleted:^{
+        XCTFail(@"This signal is not supposed to complete.");
+    }]
+    skip:1]
+    takeUntilBlock:^BOOL (CBLQueryEnumerator *rows) {
+        BOOL result = rows.allObjects.count == 1;
+        NSLog(@"Filter condition %@matched for value %@ (%@).", result ? @"" : @"un", rows, rows.allObjects);
+        return result;
+    }]
+    timeout:5.0 description:@"observed initial value for rows"];
+    [self asynchronouslyPostTrivialChangeToDocumentWithID:[[NSUUID UUID] UUIDString]];
+    [self rcl_expectCompletionFromSignal:[[liveQuerySignal
+    skip:1]
+    takeUntilBlock:^BOOL (CBLQueryEnumerator *rows) {
+        BOOL result = rows.allObjects.count == 2;
+        NSLog(@"Filter condition %@matched for value %@ (%@).", result ? @"" : @"un", rows, rows.allObjects);
+        return result;
+    }]
+    timeout:5.0 description:@"observed initial value for rows"];
+    NSLog(@"%@", liveQuerySignal);
+}
+
+- (void)testChanges {
+    RACSignal *liveQuerySignal = [[[_manager rcl_databaseNamed:_databaseName]
+    flattenMap:^RACSignal *(CBLDatabase *database) {
+        return [database rcl_allDocumentsQuery];
+    }]
+    flattenMap:^RACSignal *(CBLQuery *query) {
+        CBLLiveQuery *liveQuery = query.asLiveQuery;
+        return [liveQuery rcl_changes];
+    }];
+    NSString *UUID1 = [[NSUUID UUID] UUIDString];
+    [self asynchronouslyPostTrivialChangeToDocumentWithID:UUID1];
+    [self rcl_expectCompletionFromSignal:[[liveQuerySignal
+    doCompleted:^{
+        XCTFail(@"This signal is not supposed to complete.");
+    }]
+    takeUntilBlock:^BOOL (CBLQueryRow *row) {
+        BOOL result = [row.key isEqualToString:UUID1];
+        return result;
+    }]
+    timeout:5.0 description:@"observed first added row"];
+    NSString *UUID2 = [[NSUUID UUID] UUIDString];
+    [self asynchronouslyPostTrivialChangeToDocumentWithID:UUID2];
+    [self rcl_expectCompletionFromSignal:[[[liveQuerySignal
+    doCompleted:^{
+        XCTFail(@"This signal is not supposed to complete.");
+    }]
+    takeUntilBlock:^BOOL (CBLQueryRow *row) {
+        BOOL result = [row.key isEqualToString:UUID2];
+        return result;
+    }]
+    ignoreValues]
+    timeout:5.0 description:@"observed second added row"];
+    NSString *UUID3 = [[NSUUID UUID] UUIDString];
+    [self asynchronouslyPostTrivialChangeToDocumentWithID:UUID3];
+    [self rcl_expectCompletionFromSignal:[[liveQuerySignal
+    takeUntilBlock:^BOOL (CBLQueryRow *row) {
+        BOOL result = [row.key isEqualToString:UUID3];
+        return result;
+    }]
+    ignoreValues]
+    timeout:5.0 description:@"observed third added row"];
+    NSLog(@"%@", liveQuerySignal);
 }
 
 @end
-
-/**
-- (RACSignal *)rcl_rows;
- */
--- ReactiveCouchbaseLite/CBLLiveQuery+ReactiveCouchbaseLite.m
+++ ReactiveCouchbaseLite/CBLLiveQuery+ReactiveCouchbaseLite.m
@@ -13,20 +13,25 @@
 @implementation CBLLiveQuery (ReactiveCouchbaseLite)
 
 - (RACSignal *)rcl_rows {
-    RACSignal *result = [RACObserve(self, rows)
+    RACSignal *result = [[[RACObserve(self, rows)
+    ignore:nil]
+    initially:^{
+        [self.rcl_scheduler schedule:^{
+            NSAssert(self.rcl_isOnScheduler, @"not on correct scheduler");
+            [self start];
+        }];
+    }]
     deliverOn:self.rcl_scheduler];
     return [result setNameWithFormat:@"[%@] -rcl_rows", result.name];
 }
 
 - (RACSignal *)rcl_changes {
-    RACSignal *result = [[[[self rcl_rows]
-    deliverOn:self.rcl_scheduler]
+    RACSignal *result = [[[self rcl_rows]
     ignore:nil]
     flattenMap:^RACSignal *(CBLQueryEnumerator *enumerator) {
+        NSAssert(self.rcl_isOnScheduler, @"not on correct scheduler");
         static UInt64 lastSequence = 0;
-        RACSignal *result = [[enumerator.allObjects.rac_sequence filter:^BOOL(CBLQueryRow *row) {
-            return row.sequenceNumber > lastSequence;
-        }] signal];
+        RACSignal *result = enumerator.rcl_sequence.signal;
         lastSequence = enumerator.sequenceNumber;
         return result;
     }];
