31d194a5f4ff65996080a2453ea252617b597e6d
Nathan Douglas <ndouglas@devontechnologies.com>
2015-01-21 08:07:24 -0600

    Tests CBLQuery (ReactiveCouchbaseLite).

--- ReactiveCouchbaseLite/CBLQuery+ReactiveCouchbaseLite.Tests.m
+++ ReactiveCouchbaseLite/CBLQuery+ReactiveCouchbaseLite.Tests.m
@@ -11,7 +11,7 @@
 #import "RCLTestDefinitions.h"
 #import "RCLDefinitions.h"
 
-@interface CBLQuery_ReactiveCouchbaseLiteTests : XCTestCase
+@interface CBLQuery_ReactiveCouchbaseLiteTests : RCLTestCase
 
 @end
 
@@ -19,20 +19,38 @@
 
 - (void)setUp {
 	[super setUp];
+    [self rcl_setupDatabase];
 }
 
 - (void)tearDown {
+    [self rcl_tearDown];
 	[super tearDown];
 }
 
-- (void)test {
-	/*
-		Run a test here.
-	*/
+- (void)testRun {
+    NSError *error = nil;
+    for (int i = 0; i < 500; i++) {
+        [[self.testDatabase documentWithID:[[NSUUID UUID] UUIDString]] update:^BOOL(CBLUnsavedRevision *unsavedRevision) {
+            unsavedRevision.properties[[[NSUUID UUID] UUIDString]] = [[NSUUID UUID] UUIDString];
+            unsavedRevision.properties[[[NSUUID UUID] UUIDString]] = [[NSUUID UUID] UUIDString];
+            return YES;
+        } error:&error];
+    }
+    [self rcl_expectNext:^(CBLQueryEnumerator *queryEnumerator) {
+        XCTAssertTrue(queryEnumerator.count == 500);
+    } signal:[[self.testDatabase createAllDocumentsQuery] rcl_run]  timeout:5.0 description:@"query returned with appropriate number of results"];
+}
+
+- (void)testFlattenedRows {
+    NSError *error = nil;
+    for (int i = 0; i < 500; i++) {
+        [[self.testDatabase documentWithID:[[NSUUID UUID] UUIDString]] update:^BOOL(CBLUnsavedRevision *unsavedRevision) {
+            unsavedRevision.properties[[[NSUUID UUID] UUIDString]] = [[NSUUID UUID] UUIDString];
+            unsavedRevision.properties[[[NSUUID UUID] UUIDString]] = [[NSUUID UUID] UUIDString];
+            return YES;
+        } error:&error];
+    }
+    [self rcl_expectCompletionFromSignal:[[[[self.testDatabase createAllDocumentsQuery] rcl_flattenedRows] take:500] ignoreValues] timeout:5.0 description:@"query returned with appropriate number of results"];
 }
 
 @end
-
-/**
-- (RACSignal *)rcl_run;
- */
