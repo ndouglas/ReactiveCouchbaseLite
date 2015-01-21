9bb2723b08d8d72efab09f9475801966a993aa8b
Nathan Douglas <ndouglas@devontechnologies.com>
2015-01-21 08:16:20 -0600

    Improves CBLReplication (ReactiveCouchbaseLite) tests.

--- ReactiveCouchbaseLite/CBLReplication+ReactiveCouchbaseLite.Tests.m
+++ ReactiveCouchbaseLite/CBLReplication+ReactiveCouchbaseLite.Tests.m
@@ -11,19 +11,7 @@
 #import "ReactiveCouchbaseLite.h"
 #import "RCLTestDefinitions.h"
 
-@interface CBLReplication_ReactiveCouchbaseLiteTests : XCTestCase {
-    CBLManager *_manager;
-    NSUInteger _port;
-    NSNumber *_testID;
-    NSString *_testName;
-    NSString *_peerName;
-    CBLDatabase *_testDatabase;
-    CBLDatabase *_peerDatabase;
-    CBLReplication *_pullReplication;
-    CBLReplication *_pushReplication;
-    CBLListener *_listener;
-    NSURL *_peerURL;
-}
+@interface CBLReplication_ReactiveCouchbaseLiteTests : RCLTestCase
 
 @end
 
@@ -31,57 +19,27 @@
 
 - (void)setUp {
 	[super setUp];
-    _port = 2014;
-    [CBLManager rcl_enableUsefulLogs];
-    _manager = [CBLManager sharedInstance];
-    _testID = @([[[NSUUID UUID] UUIDString] hash]);
-    _testName = [NSString stringWithFormat:@"test_%@", _testID];
-    _peerName = [NSString stringWithFormat:@"peer_%@", _testID];
-    _listener = [[CBLListener alloc] initWithManager:_manager port:_port];
-    NSError *error = nil;
-    XCTAssertTrue([_listener start:&error], @"Error: %@", error);
-    XCTAssertTrue(_testDatabase = [_manager databaseNamed:_testName error:&error], @"Error: %@", error);
-    [_testDatabase createAllDocumentsQuery];
-    XCTAssertTrue(_peerDatabase = [_manager databaseNamed:_peerName error:&error], @"Error: %@", error);
-    [_peerDatabase createAllDocumentsQuery];
-    _peerURL = [_listener.URL URLByAppendingPathComponent:_peerDatabase.name];
-    XCTAssertTrue(_pullReplication = [_testDatabase createPullReplication:_peerURL], @"Error: %@", error);
-    _pullReplication.continuous = YES;
-    [_pullReplication start];
-    XCTAssertTrue(_pushReplication = [_testDatabase createPushReplication:_peerURL], @"Error: %@", error);
-    _pushReplication.continuous = YES;
-    [_pushReplication start];
+    [self rcl_setupEverything];
 }
 
 - (void)tearDown {
-    NSError *error = nil;
-    [_pushReplication stop];
-    _pushReplication = nil;
-    [_pullReplication stop];
-    _pullReplication = nil;
-    [_listener stop];
-    _listener = nil;
-    XCTAssertTrue([_testDatabase deleteDatabase:&error], @"Error: %@", error);
-    _testDatabase = nil;
-    XCTAssertTrue([_peerDatabase deleteDatabase:&error], @"Error: %@", error);
-    _peerDatabase = nil;
-    _manager = nil;
+    [self rcl_tearDown];
 	[super tearDown];
 }
 
 - (void)testPendingPushDocumentIDs {
 	NSString *documentID = [[NSUUID UUID] UUIDString];
-    [self rcl_triviallyUpdateDocument:[_testDatabase documentWithID:documentID] times:1 interval:1];
+    [self rcl_triviallyUpdateDocument:[self.testDatabase documentWithID:documentID] times:1 interval:1];
     [self rcl_expectNexts:@[
         ^(NSString *pendingDocumentID) {
             XCTAssertTrue([pendingDocumentID isEqualToString:documentID]);
         },
-    ] signal:[[_pushReplication rcl_pendingPushDocumentIDs] take:1] timeout:5.0 description:@"pending push document IDs received correctly"];
+    ] signal:[[self.pushReplication rcl_pendingPushDocumentIDs] take:1] timeout:5.0 description:@"pending push document IDs received correctly"];
 }
 
 - (void)testTransferredDocuments {
 	NSString *documentID = [[NSUUID UUID] UUIDString];
-    [self rcl_triviallyUpdateDocument:[_testDatabase documentWithID:documentID] times:3 interval:1];
+    [self rcl_triviallyUpdateDocument:[self.testDatabase documentWithID:documentID] times:3 interval:1];
     [self rcl_expectNexts:@[
         ^(NSDictionary *transferringDocument) {
             XCTAssertTrue([transferringDocument[@"_id"] isEqualToString:documentID]);
@@ -92,7 +50,7 @@
         ^(NSDictionary *transferringDocument) {
             XCTAssertTrue([transferringDocument[@"_id"] isEqualToString:documentID]);
         },
-    ] signal:[[_pushReplication rcl_transferredDocuments] take:3] timeout:5.0 description:@"pending push document IDs received correctly"];
+    ] signal:[[self.pushReplication rcl_transferredDocuments] take:3] timeout:5.0 description:@"pending push document IDs received correctly"];
 }
 
 @end
