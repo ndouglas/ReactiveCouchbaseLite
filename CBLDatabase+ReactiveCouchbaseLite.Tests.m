b804250d8a39f9c0edffc404f836d3c839f7cc97
Nathan Douglas <ndouglas@devontechnologies.com>
2015-08-27 12:10:24 -0500

    1.0.48

--- CBLDatabase+ReactiveCouchbaseLite.Tests.m
+++ CBLDatabase+ReactiveCouchbaseLite.Tests.m
@@ -11,7 +11,7 @@
 #import "RCLTestDefinitions.h"
 #import "ReactiveCouchbaseLite.h"
 #import <ReactiveCocoa/ReactiveCocoa.h>
-#import <kern/OSAtomic.h>
+#import <libkern/OSAtomic.h>
 
 typedef void (^RCLObjectTesterBlock)(id);
 typedef RCLObjectTesterBlock (^RCLObjectTesterGeneratorBlock)(id);
